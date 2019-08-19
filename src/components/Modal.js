210f642eaeae11d6ffab54e4b0396686de1d382c
Anna Fulton <afulton7@gmail.com>
2019-08-19 10:28:25 -0400

    Finish first full draft of modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,29 +2,17 @@ import React, {useState} from 'react';
 const titleCase = require('../utils/titleCase');
 
 const Modal = ({plant, handlePlantIt, toggleActive, isActive}) => {
-  const getPlantCompanions = () => {
-    /** 🚨  
-     * Fix this on Monday
-     * Turn an array of strings into one long string with commmas
-    */
-    if (!plant.companions) {
-      return '';
-    } else if (plant.companions.length === 1) {
-      return titleCase(plant.companions[0]);
-    } else if (plant.companions.length === 2) {
-      return `${titleCase(
-        plant.companions[0]
-      )} and ${plant.companions[1].toLowerCase()}`;
-    } else {
-      return 'More than 2';
-    }
-    const first = titleCase(plant.companions.shift());
-    const last = plant.companions.pop();
-    const middle = plant.companions
+  const convertArray = (obj, prop) => {
+    const array = obj[prop];
+    if (!array) return '';
+    if (array.length === 1) return titleCase(array[0]);
+    if (array.length === 2)
+      return `${titleCase(array[0])} and ${array[1].toLowerCase()}`;
+    const middle = array
       .slice(1, -1)
       .map(item => item.toLowerCase())
       .join(', ');
-    return `${first}, ${middle}and ${last}`;
+    return `${titleCase(array[0])}, ${middle}, and ${array[array.length - 1]}`;
   };
 
   return (
@@ -59,7 +47,59 @@ const Modal = ({plant, handlePlantIt, toggleActive, isActive}) => {
             </p>
             <p>
               <span className='subtitle is-5'>Plant next to: </span>
-              {getPlantCompanions()}
+              {convertArray(plant, 'companions')}
+            </p>
+            <p>
+              <span className='subtitle is-5'>Don't plant next to: </span>
+              {convertArray(plant, 'enemies')}
+            </p>
+            <p>
+              <span className='subtitle is-5'>Spacing between plants: </span>
+              {plant.spacingBtwnPlants_in} inches
+            </p>
+            <p>
+              <span className='subtitle is-5'>Spacing between rows: </span>
+              {plant.spacingBtwnPlants_in} inches
+            </p>
+            <p>
+              <span className='subtitle is-5'>Water: </span>
+              {titleCase(plant.water)}
+            </p>
+            <p>
+              <span className='subtitle is-5'>Fertilizer: </span>
+              {plant.fertilizer}
+            </p>
+            <p>
+              <span className='subtitle is-5'>Diseases: </span>
+              {convertArray(plant, 'diseases')}
+            </p>
+            <p>
+              <span className='subtitle is-5'>Pests: </span>
+              {convertArray(plant, 'pests')}
+            </p>
+            <p>
+              <span className='subtitle is-5'>Start seeds indoors: </span>
+              {titleCase(plant.startSeedsIndoors)}
+            </p>
+            <p>
+              <span className='subtitle is-5'>Transplant: </span>
+              {titleCase(plant.transplant)}
+            </p>
+            <p>
+              <span className='subtitle is-5'>Direct sow: </span>
+              {titleCase(plant.directSow)}
+            </p>
+            <p>
+              <span className='subtitle is-5'>Days to germination: </span>
+              {plant.daysToGermination}
+            </p>
+            <p>
+              <span className='subtitle is-5'>Days until harvest: </span>
+              {plant.daysToHarvest}
+            </p>
+            <p>
+              <span className='subtitle is-5'>Other tips: </span>
+              {titleCase(plant.otherCare)}
             </p>
           </div>
         </section>
